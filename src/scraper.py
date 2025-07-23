38704f09c21fd6666f352b8e3241096284237742
maja-burrack <113234765+maja-burrack@users.noreply.github.com>
2025-07-23 20:20:57 +0200

    wrapped the scraper in a class

--- src/scraper.py
+++ src/scraper.py
@@ -11,149 +11,169 @@ from typing import List
 import os
 from dotenv import load_dotenv
 
-def _decompress_response(response):
-    raw = response.content
-    encoding = response.headers.get("Content-Encoding")
-    
-    if encoding == "br":
-        decoded = brotli.decompress(raw)
-    elif encoding == "gzip":
-        decoded = gzip.decompress(raw)
-    else:
-        decoded = raw
-    return decoded
+class IFSCResultsScraper:
+    def __init__(self, baseurl: str, headers: dict):
+        self.baseurl = baseurl
+        self.headers = headers
 
-def get_event_ids(year, headers):
-    season_id = _map_year_to_season_id(year)
-    url = BASEURL + f"seasons/{season_id}"
+    def _test_api(self, test_season_id=37):
+        url = self.baseurl + f"seasons/{test_season_id}"
+        response = requests.get(url, headers=self.headers)
+        if response.status_code == 200:
+            print("Response OK!")
+        else:
+            print(f"ERROR! Status code {response.status_code}")
     
-    response = requests.get(url, headers=headers)
-    assert response.status_code == 200, f"Error code {response.status_code}!"
-    
-    decoded = _decompress_response(response)
-    
-    r = json.loads(decoded)
+    def get_event_ids(self, year):
+        season_id = self._map_year_to_season_id(year)
+        url = self.baseurl + f"seasons/{season_id}"
+        
+        response = requests.get(url, headers=self.headers)
+        assert response.status_code == 200, f"Error code {response.status_code}!"
+        
+        decoded = self._decompress_response(response)
+        
+        r = json.loads(decoded)
 
-    league_id = r['leagues'][0]['url'].split('/')[-1]
+        league_id = r['leagues'][0]['url'].split('/')[-1]
 
-    event_ids = [
-        event['event_id']
-        for event in r['events']
-        if event['league_season_id'] == int(league_id)
-    ]
-    
-    return event_ids
+        event_ids = [
+            event['event_id']
+            for event in r['events']
+            if event['league_season_id'] == int(league_id)
+        ]
+        
+        return event_ids
 
-def _map_year_to_season_id(year):
-    start_year = 1990
-    season_id = year - start_year + 2
-    return season_id 
+    @staticmethod
+    def _decompress_response(response):
+        raw = response.content
+        encoding = response.headers.get("Content-Encoding")
+        
+        try:
+            if encoding == "br":
+                decoded = brotli.decompress(raw)
+            elif encoding == "gzip":
+                decoded = gzip.decompress(raw)
+            else:
+                decoded = raw
+        except Exception as e:
+            print(f"Decompression failed: {e}. Falling back to raw content.")
+            decoded = raw
+        return decoded
 
-def get_event_dcat_ids(event_id, headers):
-    url = BASEURL + f"events/{event_id}"
-    response = requests.get(url, headers=headers)
-    
-    response = _decompress_response(response)
-    response = json.loads(response)
-    
-    # get ids for dcats
-    dcat_ids = [
-        dcat['dcat_id'] for dcat in response['dcats']
-    ]
+    @staticmethod
+    def _map_year_to_season_id(year: int):
+        start_year = 1990
+        season_id = year - start_year + 2
+        return season_id 
 
-    return dcat_ids
+    def get_event_dcat_ids(self, event_id: int):
+        url = self.baseurl + f"events/{event_id}"
+        response = requests.get(url, headers=self.headers)
+        
+        response = self._decompress_response(response)
+        response = json.loads(response)
+        
+        # get ids for dcats
+        dcat_ids = [
+            dcat['dcat_id'] for dcat in response['dcats']
+        ]
 
-def get_event_results(event_id, headers):
-    dcat_ids = get_event_dcat_ids(event_id)
-    
-    responses = {'event_id': event_id}
-    for dcat_id in dcat_ids:
-        url = BASEURL + f"/events/{event_id}/result/{dcat_id}"
-        results = json.loads(_decompress_response(requests.get(url, headers=headers)))
-        results['dcat_id'] = dcat_id
-        responses['results'] = results
-    
-    return responses
+        return dcat_ids
 
-def get_athlete_info(athlete_id: int, headers):
-    url = BASEURL + f"athletes/{athlete_id}"
-    response= requests.get(url, headers=headers)
-    athlete_info = json.loads(_decompress_response(response))
-    return athlete_info
+    def get_event_results(self, event_id: int):
+        dcat_ids = self.get_event_dcat_ids(event_id)
+        
+        responses = {'event_id': event_id}
+        for dcat_id in dcat_ids:
+            url = self.baseurl + f"/events/{event_id}/result/{dcat_id}"
+            results = json.loads(self._decompress_response(requests.get(url, headers=self.headers)))
+            results['dcat_id'] = dcat_id
+            responses['results'] = results
+        
+        return responses
 
-def get_athlete_info_multiple(athlete_ids: List[int], headers):
-    athlete_info = {}
-    for athlete_id in athlete_ids:
-        athlete_info[athlete_id] = get_athlete_info(athlete_id, headers=headers)
-        time.sleep(15)
-    return athlete_info
+    def get_athlete_info(self, athlete_id: int):
+        url = self.baseurl + f"athletes/{athlete_id}"
+        response= requests.get(url, headers=self.headers)
+        athlete_info = json.loads(self._decompress_response(response))
+        return athlete_info
 
-def fetch_data(year=2025, headers=headers):
-    event_ids = get_event_ids(year=year, headers=headers)
-    
-    all_event_results = []
-    for event_id in event_ids:
-        event_results = get_event_results(event_id=event_id, headers=headers)
-        all_event_results.append(event_results)
-    
-    return all_event_results
+    def get_athlete_info_multiple(self, athlete_ids: List[int]):
+        athlete_info = {}
+        for athlete_id in athlete_ids:
+            athlete_info[athlete_id] = self.get_athlete_info(athlete_id)
+            time.sleep(15)
+        return athlete_info
 
-def parse_data(data):
-    df = pd.DataFrame(data)
-    
-    df['event_name'] = df['results'].apply(lambda x: x['event'])
-    df['dcat_id'] = df['results'].apply(lambda x: x['dcat_id'])
-    df['dcat'] = df['results'].apply(lambda x: x['dcat'])
-    df['status'] = df['results'].apply(lambda x: x['status'])
-    
-    df = df[df['status']=='finished']
-    
-    df['ranking'] = df['results'].apply(lambda x: x['ranking'])
-    df = df.explode('ranking')
-    
-    df['athlete_id'] = df['ranking'].apply(lambda x: x['athlete_id'])
-    df['athlete_name'] = df['ranking'].apply(lambda x: x['name'])
-    df['athlete_country'] = df['ranking'].apply(lambda x: x['country'])
-    df['rounds'] = df['ranking'].apply(lambda x: x['rounds'])
-    
-    df = df.explode('rounds')
-    df['round'] = df['rounds'].apply(lambda x: x['round_name'])
-    df['score'] = df['rounds'].apply(lambda x: x['score'])
+    def fetch_data(self, year=2025):
+        event_ids = self.get_event_ids(year=year)
+        
+        all_event_results = []
+        for event_id in event_ids:
+            event_results = self.get_event_results(event_id=event_id)
+            all_event_results.append(event_results)
+        
+        return all_event_results
 
-    df = df.drop(['ranking', 'rounds', 'results'], axis=1)
-    
-    return df
+    @staticmethod
+    def parse_data(data):
+        df = pd.DataFrame(data)
+        
+        df['event_name'] = df['results'].apply(lambda x: x['event'])
+        df['dcat_id'] = df['results'].apply(lambda x: x['dcat_id'])
+        df['dcat'] = df['results'].apply(lambda x: x['dcat'])
+        df['status'] = df['results'].apply(lambda x: x['status'])
+        
+        df = df[df['status']=='finished']
+        
+        df['ranking'] = df['results'].apply(lambda x: x['ranking'])
+        df = df.explode('ranking')
+        
+        df['athlete_id'] = df['ranking'].apply(lambda x: x['athlete_id'])
+        df['athlete_name'] = df['ranking'].apply(lambda x: x['name'])
+        df['athlete_country'] = df['ranking'].apply(lambda x: x['country'])
+        df['rounds'] = df['ranking'].apply(lambda x: x['rounds'])
+        
+        df = df.explode('rounds')
+        df['round'] = df['rounds'].apply(lambda x: x['round_name'])
+        df['score'] = df['rounds'].apply(lambda x: x['score'])
 
-def transform_data(df, only_finalists=True):
-    df = df.loc[df['dcat'].str.contains('boulder', case=False)]
-    
-    df['comp_id'] = df['event_id'].astype(str) + df['dcat_id'].astype(str)
-    
-    if only_finalists:
-        athletes_in_final = df[df['round']=='Final'][['comp_id', 'athlete_id']].drop_duplicates()
-        df = df.merge(athletes_in_final, on=['comp_id', 'athlete_id'], how='inner')
-    
-    df = df.drop('status', axis=1)
-    
-    df = _enrich_with_athlete_data(df)
-    
-    return df
+        df = df.drop(['ranking', 'rounds', 'results'], axis=1)
+        
+        return df
 
-def _enrich_with_athlete_data(df):
-    athlete_ids = df['athlete_id'].unique().tolist()
-    athlete_info = get_athlete_info_multiple(athlete_ids=athlete_ids)
-    
-    athlete_df = pd.DataFrame(athlete_info.values())
-    
-    athlete_df.rename({'id': 'athlete_id'}, axis=1, inplace=True)
-    
-    athlete_df['first_season'] = athlete_df['all_results'].apply(lambda x: min([int(i['season']) for i in x]))
-    
-    athlete_df = athlete_df.loc[:, ["athlete_id", "birthday", "gender", "first_season"]]
-    
-    df_enriched = df.merge(athlete_df, on="athlete_id", how='left')
-    
-    return df_enriched
+    def transform_data(self, df, only_finalists=True):
+        df = df.loc[df['dcat'].str.contains('boulder', case=False)]
+        
+        df['comp_id'] = df['event_id'].astype(str) + df['dcat_id'].astype(str)
+        
+        if only_finalists:
+            athletes_in_final = df[df['round']=='Final'][['comp_id', 'athlete_id']].drop_duplicates()
+            df = df.merge(athletes_in_final, on=['comp_id', 'athlete_id'], how='inner')
+        
+        df = df.drop('status', axis=1)
+        
+        df = self._enrich_with_athlete_data(df)
+        
+        return df
+
+    def _enrich_with_athlete_data(self, df):
+        athlete_ids = df['athlete_id'].unique().tolist()
+        athlete_info = self.get_athlete_info_multiple(athlete_ids=athlete_ids)
+        
+        athlete_df = pd.DataFrame(athlete_info.values())
+        
+        athlete_df.rename({'id': 'athlete_id'}, axis=1, inplace=True)
+        
+        athlete_df['first_season'] = athlete_df['all_results'].apply(lambda x: min([int(i['season']) for i in x]))
+        
+        athlete_df = athlete_df.loc[:, ["athlete_id", "birthday", "gender", "first_season"]]
+        
+        df_enriched = df.merge(athlete_df, on="athlete_id", how='left')
+        
+        return df_enriched
 
     
 if __name__ == "__main__":
@@ -170,10 +190,11 @@ if __name__ == "__main__":
     headers = config['headers']
     headers['cookie'] = COOKIE
     
-    data = fetch_data(year=YEAR, headers=headers)
+    scraper = IFSCResultsScraper(headers=headers, baseurl=BASEURL)
     
-    df = parse_data(data)
-    df_transformed = transform_data(df, only_finalists=True)
+    data = scraper.fetch_data(year=YEAR)
+    df = scraper.parse_data(data)
+    df_transformed = scraper.transform_data(df, only_finalists=True)
 
     folder_dest = "../data"
     if not os.path.isdir(folder_dest):
